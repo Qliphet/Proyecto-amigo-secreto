9b3ba0c0b221aaf1ffc32127a238c977fca82e70
Hector Corona <khirvy.hc@gmail.com>
2025-09-22 14:49:09 -0600

    Se termina de limpiar texto innecesario, así como se agregan comentarios para que sea más sencillo el entendimiento del código

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
+// Este programa te dará de manera aleatoria el nombre de un amigo de una lista que tú selecciones.
 let listaDeAmigos = [];
 let amigoDeUsuario = '';
 let amigoSecreto = '';
 let sorteo = true;
+
+
 
 //Función auxiliar para asignar texto a un elemento HTML
 function asignarTextoElemento(elemento, texto) {
@@ -13,9 +15,54 @@ function asignarTextoElemento(elemento, texto) {
 }
 
 
+//Función auxiliar para limpiar la caja de texto
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 }
+
+
+// Función que agrega amigos a la lista y borra la caja de texto
+function agregarAmigo() {
+    asignarTextoElemento('h2','Puedes agregar más amigos si quieres');
+    amigoDeUsuario = document.getElementById('amigo').value;
+    limpiarCaja();
+    if (listaDeAmigos.includes(amigoDeUsuario)) {
+        asignarTextoElemento('h2', 'Ese amigo ya está en la lista elije a otro');
+    } else {
+        if (amigoDeUsuario === '') {
+            asignarTextoElemento('h2', 'No has ingresado ningún nombre, ingresa uno');
+        }
+        else {
+        listaDeAmigos.push(amigoDeUsuario);
+        actualizarLista();
+        document.querySelector('.button-draw').removeAttribute('disabled');
+        }
+    }
+}
+
+
+// Agregar un evento que active agregarAmigo() al presionar Enter
+document.addEventListener('DOMContentLoaded', function() {
+    const input = document.getElementById('amigo');
+    input.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            agregarAmigo();
+        }
+    });
+});
+
+
+//Función para actualizar la lista en el HTML
+function actualizarLista() {
+    let listaHTML = document.querySelector('ul');
+    listaHTML.innerHTML = '';
+    listaDeAmigos.forEach(function(amigo) {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        listaHTML.appendChild(li);
+    });
+}
+
 
 //Función que elije un amigo al azar y reinicia el sorteo
 function sortearAmigo() {
@@ -39,25 +86,6 @@ function sortearAmigo() {
 }
 
 
-// Función que agrega amigos a la lista y borra la caja de texto
-function agregarAmigo() {
-    asignarTextoElemento('h2','Puedes agregar más amigos si quieres');
-    amigoDeUsuario = document.getElementById('amigo').value;
-    limpiarCaja();
-    if (listaDeAmigos.includes(amigoDeUsuario)) {
-        asignarTextoElemento('h2', 'Ese amigo ya está en la lista elije a otro');
-    } else {
-        if (amigoDeUsuario === '') {
-            asignarTextoElemento('h2', 'No has ingresado ningún nombre, ingresa uno');
-        }
-        else {
-        listaDeAmigos.push(amigoDeUsuario);
-        actualizarLista();
-        document.querySelector('.button-draw').removeAttribute('disabled');
-        }
-    }
-}
-
 // Función que reinicia el sorteo
 function reiniciarSorteo() {
     listaDeAmigos = [];
@@ -72,105 +100,3 @@ function reiniciarSorteo() {
     sorteo = true;
     document.querySelector('#amigo').setAttribute('disabled','false')
 }
-
-// Agregar un evento que active agregarAmigo() al presionar Enter
-document.addEventListener('DOMContentLoaded', function() {
-    const input = document.getElementById('amigo');
-    input.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-            agregarAmigo();
-        }
-    });
-});
-
-function actualizarLista() {
-    let listaHTML = document.querySelector('ul');
-    listaHTML.innerHTML = '';
-    listaDeAmigos.forEach(function(amigo) {
-        let li = document.createElement('li');
-        li.textContent = amigo;
-        listaHTML.appendChild(li);
-    });
-}
-
-
-
-
-/*Pedazo de código del juego del número secreto
-
-let numeroSecreto = 0;
-let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
-
-
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
-    if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
-    } else {
-        //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
-        } else {
-            asignarTextoElemento('p','El número secreto es mayor');
-        }
-        intentos++;
-        limpiarCaja();
-    }
-    return;
-}
-
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
-}
-
-function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
-
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
-    //Si ya sorteamos todos los números
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
-    } else {
-        //Si el numero generado está incluido en la lista 
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
-            return generarNumeroSecreto();
-        } else {
-            listaNumerosSorteados.push(numeroGenerado);
-            return numeroGenerado;
-        }
-    }
-}
-
-function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-    console.log(numeroSecreto);
-}
-
-function reiniciarJuego() {
-    //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
-    condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
-}
-
-condicionesIniciales();*/
